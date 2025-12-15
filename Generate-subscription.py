d1b58c43c8f30db766b9b0962cbdcb765a1b3460
patterniha <71074308+patterniha@users.noreply.github.com>
2025-12-15 21:37:20 +0330

    Update Generate-subscription.py

--- Generate-subscription.py
+++ Generate-subscription.py
@@ -1,22 +1,14 @@
 import json
 import os
 import json5
-from jsmin import jsmin
 
 all_configs = ["Serverless.jsonc", "Serverless-local.jsonc", "Serverless-dynx.jsonc"]
 output_path = os.path.join('Subscription', 'Serverless-for-Iran.json')
 
-all_json5 = []
-all_jsmin = []
+all_j = []
 for config in all_configs:
     with open(config) as config_file:
-        all_json5.append(json5.load(config_file))
-        config_file.seek(0)
-        all_jsmin.append(json.loads(jsmin(config_file.read())))
+        all_j.append(json5.load(config_file))
 
-if all_json5 != all_jsmin:
-    print("error")
-
-else:
-    json.dump(all_json5, open(output_path, "w"), indent=4)
-    print("done")
+json.dump(all_j, open(output_path, "w"), indent=4)
+print("done")
